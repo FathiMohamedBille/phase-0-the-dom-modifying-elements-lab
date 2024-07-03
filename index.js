1223d9aefc99814a4a72828f65774eba660d5848
fathimohamed <mohafathi24@gmail.com>
2024-07-03 17:45:12 +0300

    dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const mainElement=document.getElementById("main");
+if (mainElement) {
+    mainElement.remove();
+}
+const newHeader=document.createElement("h1");
+newHeader.id="victory";
+const yourName="Fathi Moha";
+newHeader.textContent=`$ {FathiMoha}is the champion`;
